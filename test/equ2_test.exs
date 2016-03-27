f21615c4d0afc06873253b2ac9fd666f7c3d11b7
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-03-27 04:38:30 -0300

    Pequena alteração no teste.

--- test/equ2_test.exs
+++ test/equ2_test.exs
@@ -2,7 +2,7 @@ defmodule Equ2Test do
   use ExUnit.Case
   doctest Equ2
 
-  test "the truth" do
+  test "Equ2 Test" do
     assert Equ2.calc(1, 4, 4) == {0, -2, -2}
     assert Equ2.calc(1, 1, 1) == {-3, nil, nil}
   end
