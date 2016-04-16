2d8b26f33a09ae2c249e33b9d18d74bd7e652adc
Isaac Pinheiro <isaacpnhr@gmail.com>
2016-04-16 16:49:40 -0300

    Pequena correção.

--- lib/equ2.ex
+++ lib/equ2.ex
@@ -1,22 +1,22 @@
 defmodule Equ2 do
 
   def calc(a, b, c) do
-    delta = calcDelta(a, b, c)
-    {x1, x2} = calcX(a, b, delta)
+    delta = calc_delta(a, b, c)
+    {x1, x2} = calc_x(a, b, delta)
     {delta, x1, x2}
   end
 
-  defp calcDelta(a, b, c) do
+  defp calc_delta(a, b, c) do
     ((:math.pow(b, 2))-(4*a*c))
   end
 
-  defp calcX(a, b, delta) when delta >= 0 do
+  defp calc_x(a, b, delta) when delta >= 0 do
     x1 = ((-b+:math.sqrt(delta))/(2*a))
     x2 = ((-b-:math.sqrt(delta))/(2*a))
     {x1, x2}
   end
 
-  defp calcX(_, _, delta) do
+  defp calc_x(_, _, delta) do
     {nil, nil}
   end
 
